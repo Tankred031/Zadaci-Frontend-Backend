25cbf2116898bf9abe141bf50db87a32ca8bd7a3
Tankred031 <tankred.kralj@gmail.com>
2026-06-06 16:06:22 +0200

    Korekcije

--- 001_Frontend/02_Unit_Converter/script.js
+++ 001_Frontend/02_Unit_Converter/script.js
@@ -1,20 +1,29 @@
 function convert() {
-    const inputValue = document.getElementById("userInput").value;
-    const unit = document.getElementById("unit").value;
-    const milesToKm = unit === 'milesToKm';
+
+    let inputValue = document.getElementById("userInput").value;
+    let unit = document.getElementById("unit").value;
+    const milesToKm = (unit === 'milesToKm');
 
     let result = 0;
 
     if (milesToKm === true) {
-        inputValue = result * 1.60934;
+        result = inputValue * 1.60934;
+
+        const resultString = inputValue + " miles are " + result.toFixed(3) + " km";
+        console.log(resultString);
+
+        const resultElement = document.getElementById("resultElement");
+        resultElement.innerHTML = resultString;
+
+
     } else {
-        inputValue = result / 1.60934;
+        result = inputValue / 1.60934;
     }
 
-    const resultString = inputValue + " miles are " + result + " km";
+    const resultString = inputValue + " km are " + result.toFixed(3) + " miles";
     console.log(resultString);
 
     const resultElement = document.getElementById("resultElement");
     resultElement.innerHTML = resultString;
-    
+
 }

74937814ee8bb3c69539a472623fb53b6875e2cb
Tankred031 <tankred.kralj@gmail.com>
2026-06-07 12:40:51 +0200

    Vjezba 4 - dio 2

--- 001_Frontend/04_Guessing_Game/script.js
+++ 001_Frontend/04_Guessing_Game/script.js
@@ -1,20 +1,34 @@
 let randomNumber = Math.floor(Math.random() * 100) + 1;
+let attempts = 10;
 
 function checkGuess() {
-    let inputElement = document.getElementById("guess").value;
-    let feedbackElement = document.getElementById("feedback").value;
+    attempts--;
 
-    let guess = inputElement.value;
+    const inputElement = document.getElementById("guess");
+    const feedbackElement = document.getElementById("feedback");
 
-    if (guess === randomNumber) {
-        feedbackElement.innerHTML = "Congratulations!";
-        feedbackElement.style.color = "green";
-    } else if (guess < randomNumber) {
-        feedbackElement.innerHTML = "Too low! Try again.";
-        feedbackElement.style.color = "red";
-    } else {
-        feedbackElement.innerHTML = "Too high! Try again.";
+    const guess = inputElement.value;
+
+    while (attempts > 0) {
+        if (guess === randomNumber) {
+            attempts = 0;
+            feedbackElement.innerHTML = "Congratulations!";
+            feedbackElement.style.color = "green";
+            break;
+        } else if (guess < randomNumber) {
+            feedbackElement.innerHTML = "Too low! Try again.";
+            feedbackElement.style.color = "red";
+        } else {
+            feedbackElement.innerHTML = "Too high! Try again.";
+            feedbackElement.style.color = "red";
+        }
+        
+    }
+
+    if (attempts === 0 && guess !== randomNumber) {
+        feedbackElement.innerHTML = `Sorry, you're out of attempts! The correct number was ${randomNumber}`;
         feedbackElement.style.color = "red";
     }
+
 }
 

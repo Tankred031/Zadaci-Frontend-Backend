9aea3778443d472344aa241450efe043405c3ea9
Tankred031 <tankred.kralj@gmail.com>
2026-06-07 13:03:16 +0200

    Vjezba 1b - dio 2

--- 002_Backend/01_Light_Switch_Program/script.js
+++ 002_Backend/01_Light_Switch_Program/script.js
@@ -1,10 +1,18 @@
 const isDay = false;
 const isLightOn = !isDay;
 
+let batteryLevel = 50;
+const isWorking = isLightOn && batteryLevel > 0;
+
+
 console.log("Daytime?");
 console.log(isDay);
 console.log("Lights On?");
 console.log(isLightOn);
+
+console.log(batteryLevel + "%");
+console.log("Everything working?");
+console.log(isWorking);
 
 
 

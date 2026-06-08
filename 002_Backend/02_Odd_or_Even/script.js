9a5a3ad06204bf70c07420c93eb83825a08723a9
Tankred031 <tankred.kralj@gmail.com>
2026-06-08 19:02:13 +0200

    Vježba 2b - dio 2

--- 002_Backend/02_Odd_or_Even/script.js
+++ 002_Backend/02_Odd_or_Even/script.js
@@ -1,31 +1,42 @@
 const input = require("@mimo-org/input");
 
-let playerChoice = input("Choose 'odd' or 'even': ");
-let playerNumber = parseInt(input("Choose a number between 1 and 5: "));
 
-let computerNumber = Math.floor(Math.random() * 5) + 1;
-let computerChoice = "";
-if (playerChoice === "odd") {
-  computerChoice = "even";
-} else {
-  computerChoice = "odd";
-}
+let playerWins = 0;
+let computerWins = 0;
+let rounds = 0
 
-let sum = playerNumber + computerNumber;
+while (rounds < 3) {
 
-let result = "";
-if (sum % 2 === 0) {
-  result = "even";
-} else {
-  result = "odd";
-}
+    let playerChoice = input("Choose 'odd' or 'even': ");
+    let playerNumber = parseInt(input("Choose a number between 1 and 5: "));
 
-console.log(`Player chose ${playerNumber} (${playerChoice})`);
-console.log(`Computer chose ${computerNumber} (${computerChoice})`);
-console.log(`The sum of the numbers is ${sum}, which is ${result}`);
+    let computerNumber = Math.floor(Math.random() * 5) + 1;
+    let computerChoice = "";
+    if (playerChoice === "odd") {
+        computerChoice = "even";
+    } else {
+        computerChoice = "odd";
+    }
 
-if (result === playerChoice) {
-  console.log("Player wins!");
-} else {
-  console.log("Computer wins!");
+    let sum = playerNumber + computerNumber;
+
+    let result = "";
+    if (sum % 2 === 0) {
+        result = "even";
+    } else {
+        result = "odd";
+    }
+
+    console.log(`Player chose ${playerNumber} (${playerChoice})`);
+    console.log(`Computer chose ${computerNumber} (${computerChoice})`);
+    console.log(`The sum of the numbers is ${sum}, which is ${result}`);
+
+    if (result === playerChoice) {
+        console.log("Player wins!");
+        playerWins++;
+    } else {
+        console.log("Computer wins!");
+        computerWins++;
+    }
+    rounds++;
 }

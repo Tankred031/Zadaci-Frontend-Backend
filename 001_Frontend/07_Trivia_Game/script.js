c28a1f32a70b957f3e17c6e9293de2a4ee9829a7
Tankred031 <tankred.kralj@gmail.com>
2026-06-09 17:37:41 +0200

    Vježba 7 -dio 2

--- 001_Frontend/07_Trivia_Game/script.js
+++ 001_Frontend/07_Trivia_Game/script.js
@@ -1,25 +1,36 @@
-const question = "What planet is known as the 'Red Planet'?";
-const choices = ["Earth", "Mars", "Jupiter", "Saturn"];
-const correctAnswer = "Mars";
+const questions = ["Koji planet nazivamo 'Crvenim planetom'?",
+  "Tko je naslikao Mona Lisa?"
+];
+const choicesArray = [
+  ["Zemlja", "Mars", "Jupiter", "Saturn"],
+  ["Van Gogh", "Picasso", "Da Vinci", "Rembrandt"]
+];
+const correctAnswers = ["Mars", "Da Vinci"];
+let currentQuestionIndex = 0;
+let score = 0;
 
 function displayQuestion() {
-  document.getElementById('question').innerHTML = question;
-  for (let i = 0; i < choices.length; i++){
+  if (currentQuestionIndex < questions.length) {
+    document.getElementById('question').innerHTML = questions[
+      currentQuestionIndex];
+    for (let i = 0; i < 4; i++) {
     const btn = document.getElementById(`choice${i+1}`);
-    btn.innerHTML = choices[i];
-    btn.value = choices[i];
+    btn.innerHTML = choicesArray[currentQuestionIndex][i];
+    btn.value = choicesArray[currentQuestionIndex][i];
+  }
+    } else {
+    document.getElementById('result').innerHTML =
+      `Skupio si ${score} od ${questions.length}!`;
+      document.getElementById('question').innerHTML = "";
+      document.getElementById('choices').innerHTML = "";
   }
 }
 
 function checkAnswer(button) {
-  if (button.value === correctAnswer) {
-    document.getElementById('result').innerHTML = "Correct!";
-  } else {
-    document.getElementById('result').innerHTML = "Wrong!";
-  }
-  for(let i = 1; i <= 4; i++){
-    document.getElementById(`choice${i}`).disabled = true;
-  }
+  if (button.value === correctAnswers[currentQuestionIndex]) {
+        score++;
+    }
+    currentQuestionIndex++;
+    displayQuestion();
 }
-
 displayQuestion();

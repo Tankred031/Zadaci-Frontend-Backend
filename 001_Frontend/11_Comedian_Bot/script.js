003da8860d3fcba17c9344efd7ffffadc5fe6284
Tankred031 <tankred.kralj@gmail.com>
2026-06-10 17:59:20 +0200

    Vježba 11 - dio 1

---
+++ 001_Frontend/11_Comedian_Bot/script.js
@@ -0,0 +1,37 @@
+const joke = [
+    "What did 0 say to 8?",
+    "Nice belt."
+];
+
+const chatContent = document.querySelector(".chat-content");
+
+function appendBotMessage(messageText) {
+    const messageDiv = document.createElement("div");
+    messageDiv.className = "message bot-message";
+    
+    const avatar = document.createElement("div");
+    avatar.className = "fas fa-robot message-avatar";
+    
+    const contentDiv = document.createElement("div");
+    contentDiv.className = "message-content";
+    contentDiv.textContent = messageText;
+
+    messageDiv.append(avatar, contentDiv);
+    chatContent.appendChild(messageDiv);
+    
+    chatContent.scrollTop = chatContent.scrollHeight;
+}
+
+appendBotMessage("Hello! I am Comedian Bot, here to make you laugh. Let me think of a joke.");
+
+setTimeout(function() {
+    appendBotMessage("Ok, got one.");
+}, 1000);
+
+setTimeout(function() {
+    appendBotMessage(joke[0]);
+}, 1500);
+
+setTimeout(function() {
+    appendBotMessage(joke[1]);
+}, 2000);

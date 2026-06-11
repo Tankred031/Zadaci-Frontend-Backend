19c220ab5da010ba41ecc1fe602960cc7243e32b
Tankred031 <tankred.kralj@gmail.com>
2026-06-11 21:55:40 +0200

    Vježba 6b update

--- 002_Backend/06_Media_Library/MediaItem.js
+++ 002_Backend/06_Media_Library/MediaItem.js
@@ -4,22 +4,25 @@ class Movie {
     this.title = title;
     this.director = director;
   }
+
   getDetails() {
     return `${this.title} directed by ${this.director}`;
   }
 }
+
 class FranchiseFilm extends Movie {
-  constructor(id, title, director, franchiseName, installmentNumber,
-    totalInstallments) {
+  constructor(id, title, director, franchiseName, installmentNumber, totalInstallments) {
     super(id, title, director);
     this.franchiseName = franchiseName;
     this.installmentNumber = installmentNumber;
     this.totalInstallments = totalInstallments;
   }
+
   getDetails() {
     return `${this.franchiseName} - Installment ${this.installmentNumber} of ${this.totalInstallments}: ${super.getDetails()}`;
   }
 }
+
 module.exports = {
   Movie,
   FranchiseFilm

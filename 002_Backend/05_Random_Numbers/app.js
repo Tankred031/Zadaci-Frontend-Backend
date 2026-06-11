1c8456aa11f4f7a1d5a51ea63b0a7f96f02a5fb2
Tankred031 <tankred.kralj@gmail.com>
2026-06-11 19:30:14 +0200

    Vježba 5b - dio 2

--- 002_Backend/05_Random_Numbers/app.js
+++ 002_Backend/05_Random_Numbers/app.js
@@ -1,24 +1,40 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-app.listen(port, function() {
-  console.log(`App is listening on port ${port}`);
+
+app.listen(port, function () {
+    console.log(`App is listening on port ${port}`);
 });
-app.get('/coinFlip', function(req, res) {
-  const result = Math.floor(Math.random() * 2);
-  res.status(200).json({
-    result: result === 0 ? 'Heads' : 'Tails'
-  });
+
+app.get('/coinFlip', function (req, res) {
+    const times = parseInt(req.query.times, 10) || 1;
+    let results = [];
+    for (let i = 0; i < times; i++) {
+        const result = Math.random() < 0.5 ? 'Heads' : 'Tails';
+        results.push(result);
+    }
+    res.status(200).json({ result: results });
 });
-app.get('/diceRoll', function(req, res) {
-  const result = Math.floor(Math.random() * 6) + 1;
-  res.status(200).json({
-    result: result
-  });
+
+app.get('/diceRoll', function (req, res) {
+    const times = parseInt(req.query.times) || 1;
+    let results = [];
+    for (let i = 0; i < times; i++) {
+        const result = Math.floor(Math.random() * 6) + 1;
+        results.push(result);
+    }
+    res.status(200).json({ result: results });
 });
-app.get('/randomNumber', function(req, res) {
-  const result = Math.floor(Math.random() * 100) + 1;
-  res.status(200).json({
-    result: result
-  });
+
+app.get('/randomNumber', function (req, res) {
+    const min = parseInt(req.query.min) || 1;
+    const max = parseInt(req.query.max) || 100;
+    if (min >= max) {
+        return res.status(400).json({ error: 'Min should be less than max.' });
+    }
+    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    res.status(200).json({
+        result: result
+    });
 });
+

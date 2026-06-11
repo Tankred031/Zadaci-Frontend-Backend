c919c33af9a2cab8e95fde7990c4f3256b0b9bf1
Tankred031 <tankred.kralj@gmail.com>
2026-06-11 20:25:30 +0200

    Vježba 13 - instalacija Reacta

--- 001_Frontend/13_Score_Keeper/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ScoreKeeper from "./ScoreKeeper";
-import "./App.css";
-
-const App = () => {
-  return <ScoreKeeper />;
-};
-
-export default App;
---
+++ 001_Frontend/13_Score_Keeper/src/assets/App.jsx
@@ -0,0 +1,12 @@
+import ScoreKeeper from "./ScoreKeeper.jsx";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <ScoreKeeper />
+    </div>
+  );
+}
+
+export default App;

0a8ed4bf3211f8a06306e02eef67aea9884f663a
Tankred031 <tankred.kralj@gmail.com>
2026-06-16 22:19:02 +0200

    Vježba 7b - dio 2

--- 002_Backend/07_Task_Manager/app.js
+++ 002_Backend/07_Task_Manager/app.js
@@ -14,6 +14,56 @@ app.get("/tasks", (req, res) => {
     });
 });
 
+app.post("/tasks", (req, res) => {
+    const { title, description } = req.body;
+
+    if (!title) {
+        return res.status(400).json({
+            error: "Title is required"
+        });
+    }
+
+    const result = db
+        .prepare(`
+            INSERT INTO tasks (title, description)
+            VALUES (?, ?)
+        `)
+        .run(title, description);
+
+    res.status(201).json({
+        taskId: result.lastInsertRowid
+    });
+});
+
+app.put("/tasks/:id", (req, res) => {
+    const { id } = req.params;
+    const { status } = req.body;
+
+    if (status !== "completed" && status !== "pending") {
+        return res.status(400).json({
+            error: "Invalid status"
+        });
+    }
+
+    const result = db
+        .prepare(`
+            UPDATE tasks
+            SET status = ?
+            WHERE id = ?
+        `)
+        .run(status, id);
+
+    if (result.changes === 0) {
+        return res.status(404).json({
+            error: "Task not found"
+        });
+    }
+
+    res.status(200).json({
+        message: "Task updated successfully"
+    });
+});
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
